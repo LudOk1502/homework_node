9578cf5580ca13a7ca0cbe13006858d3be85a37c
LudOk1502 <lyudmyla152@gmail.com>
2021-10-01 21:15:55 +0300

    app

--- app.js
+++ app.js
@@ -5,35 +5,40 @@ const path = require('path');
 const boysDirPath = path.join(__dirname, 'boys');
 const girlsDirPath = path.join(__dirname, 'girls');
 
-function sortByGender(directory, file) {
-    fs.readFile(`${__dirname}/${directory}/${file}`, ((err, data) => {
+function sortByGender(directory) {
+    fs.readdir(path.join(__dirname, directory), (err, files) => {
         if (err) {
             console.log(err);
             return
-        } else if (JSON.parse(data).gender === 'female') {
-            fs.rename(path.join(__dirname, directory, file),
-                path.join(girlsDirPath, file),
-                err => {
-                    console.log(err)
-                }
-            )
-        } else
-            fs.rename(path.join(__dirname, directory, file),
-                path.join(boysDirPath, file),
-                err => {
-                    console.log(err)
-                }
-            )
-    }))
+        }
+        files.forEach(file => {
+                fs.readFile(path.join(__dirname, directory, file), ((err, data) => {
+                    if (err) {
+                        console.log(err)
+                    }
+                    if (JSON.parse(data).gender === 'female') {
+                        return fs.rename(path.join(__dirname, directory, file),
+                            path.join(girlsDirPath, file),
+                            err => {
+                                if (err) {
+                                    console.log(err);
+                                }
+                            }
+                        );
+                    }
+                    return fs.rename(path.join(__dirname, directory, file),
+                        path.join(boysDirPath, file),
+                        err => {
+                            if (err) {
+                                console.log(err);
+                            }
+                        }
+                    );
+                }))
+            }
+        )
+    })
 }
 
-sortByGender('boys', 'Polina.json');
-sortByGender('boys', 'Alina.json');
-sortByGender('boys', 'Karina.json');
-sortByGender('boys', 'Oleg.json');
-sortByGender('boys', 'Viktor.json');
-sortByGender('girls', 'Max.json');
-sortByGender('girls', 'Ruslan.json');
-sortByGender('girls', 'Sofia.json');
-sortByGender('girls', 'Taras.json');
-sortByGender('girls', 'Vika.json');
+sortByGender('boys');
+sortByGender('girls');
