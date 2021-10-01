c42cb77713d18c0b981db011890acd0b4112ee60
LudOk1502 <lyudmyla152@gmail.com>
2021-10-01 19:42:45 +0300

    app

--- app.js
+++ app.js
@@ -5,21 +5,35 @@ const path = require('path');
 const boysDirPath = path.join(__dirname, 'boys');
 const girlsDirPath = path.join(__dirname, 'girls');
 
-function sortByGender(directory, new_directory, file) {
+function sortByGender(directory, file) {
     fs.readFile(`${__dirname}/${directory}/${file}`, ((err, data) => {
         if (err) {
             console.log(err);
             return
         } else if (JSON.parse(data).gender === 'female') {
-            fs.rename(path.join(__dirname, directory),
-                path.join(__dirname, new_directory, file),
+            fs.rename(path.join(__dirname, directory, file),
+                path.join(girlsDirPath, file),
                 err => {
                     console.log(err)
                 }
             )
-        }
+        } else
+            fs.rename(path.join(__dirname, directory, file),
+                path.join(boysDirPath, file),
+                err => {
+                    console.log(err)
+                }
+            )
     }))
 }
 
-
-sortByGender('boys', 'girls', 'Polina.json')
+sortByGender('boys', 'Polina.json');
+sortByGender('boys', 'Alina.json');
+sortByGender('boys', 'Karina.json');
+sortByGender('boys', 'Oleg.json');
+sortByGender('boys', 'Viktor.json');
+sortByGender('girls', 'Max.json');
+sortByGender('girls', 'Ruslan.json');
+sortByGender('girls', 'Sofia.json');
+sortByGender('girls', 'Taras.json');
+sortByGender('girls', 'Vika.json');
