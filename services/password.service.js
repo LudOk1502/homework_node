0444d2b62dc9165624033e993972fd37a2039e92
LudOk1502 <lyudmyla152@gmail.com>
2021-10-24 22:30:35 +0300

    homework lesson8

--- services/password.service.js
+++ services/password.service.js
@@ -5,8 +5,10 @@ const {errorStatus, errorMessages} = require('../configs');
 
 module.exports = {
     hash: (password) => bcrypt.hash(password, 10),
+
     compare: async (password, hashPassword) => {
         const isPasswordMatched = await bcrypt.compare(password, hashPassword);
+
         if (!isPasswordMatched) {
             throw new ErrorHandler(errorMessages.WRONG_EMAIL_OR_PASSWORD, errorStatus.STATUS_400);
         }
